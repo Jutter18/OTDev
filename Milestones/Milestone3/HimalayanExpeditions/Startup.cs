45c95080b4472a91c885af1ba18cd979bdea657e
cmorris19 <56059629+cmorris19@users.noreply.github.com>
2021-01-23 10:32:29 -0800

    Finished setup of code + implemented search bar

--- Milestones/Milestone3/HimalayanExpeditions/Startup.cs
+++ Milestones/Milestone3/HimalayanExpeditions/Startup.cs
@@ -4,10 +4,12 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using HimalayanExpeditions.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace HimalayanExpeditions
 {
@@ -21,11 +23,14 @@ namespace HimalayanExpeditions
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+       public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            IServiceCollection serviceCollections = services.AddDbContext<HimalayanExpeditionDbContext>(opts =>
+            {
+                opts.UseSqlServer(Configuration["ConnectionStrings:HimalayanExpeditions"]);
+            });
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
