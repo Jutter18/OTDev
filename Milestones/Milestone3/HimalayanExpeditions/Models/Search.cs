f78ef6fbd7ab45fdddae8b4f3530fa2bea7951e9
chrisedwou <cedwards19@mail.wou.edu>
2021-01-23 17:25:23 -0800

    Built out search to include more parameters. Added find peaks that have not been climbed by season

--- Milestones/Milestone3/HimalayanExpeditions/Models/Search.cs
+++ Milestones/Milestone3/HimalayanExpeditions/Models/Search.cs
@@ -13,7 +13,9 @@ namespace HimalayanExpeditions.Models
         [Required]
 
         public int Year { get; set; }
-
+        public int Count { get; set; }
+        public string Peak { get; set; }
+        public string Season { get; set; }
         public IEnumerable<Expedition> ExpeditionList { get; set; }
     }
 }
