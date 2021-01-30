21274ef0dcad0e201308a44ef1e149a3bd62c038
jutter18 <Jutter18@mail.wou.edu>
2021-01-30 15:39:42 -0800

    Added Ajax call to layout for live data

--- Milestones/Milestone3/HimalayanExpeditions/wwwroot/js/site.js
+++ Milestones/Milestone3/HimalayanExpeditions/wwwroot/js/site.js
@@ -7,3 +7,17 @@ console.log("Initialization check for JS");
 $("#ExpeditionSubmit").click(function () {
     alert("Expedition Submitted to Database");
 });
+
+$(document).ready(function () { window.setInterval(execute, 10000) });
+function execute() {
+    $.getJSON( '/api/count', (data) => {
+        showCount(data)
+    });
+}
+function showCount(data) {
+    $('#stat').fadeOut();
+    $('#stat').empty();
+    setTimeout(1000);
+    $('#stat').append(data.val);
+    $('#stat').fadeIn();
+}
