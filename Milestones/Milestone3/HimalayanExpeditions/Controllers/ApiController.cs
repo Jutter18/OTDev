5936c3bfc8d0fd28940709ec56abaf4611540b5d
jutter18 <Jutter18@mail.wou.edu>
2021-01-30 15:46:34 -0800

    Added Ajax call to layout for live data

---
+++ Milestones/Milestone3/HimalayanExpeditions/Controllers/ApiController.cs
@@ -0,0 +1,28 @@
+﻿using HimalayanExpeditions.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HimalayanExpeditions.Controllers
+{
+    [Route("api/{action}")]
+    [ApiController]
+    public class ApiController : Controller
+    {
+        private readonly HimalayanExpeditionDbContext db;
+        public ApiController(HimalayanExpeditionDbContext context)
+        {
+            db = context;
+        }
+        [HttpGet]
+        public IActionResult count()
+        {
+            var unclimbed = db.Peaks.Where(e => e.Expeditions.ToList().Count() == 0).ToList().Count();
+            return Json(new { val = db.Expeditions.ToList().Count().ToString() + " Expeditions to Date, with " + unclimbed + " yet to be climbed!" });
+        }
+    }
+}
