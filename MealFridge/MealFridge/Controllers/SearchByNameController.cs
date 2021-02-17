c444977c8a66fdb841bfd1b0c578a671753c8f6c
chrisedwou <cedwards19@mail.wou.edu>
2021-02-17 14:07:19 -0800

    Fixed the account managment to what was specified on tuesday meeting. Added API model for Spoonacular API call.

--- MealFridge/MealFridge/Controllers/SearchByNameController.cs
+++ MealFridge/MealFridge/Controllers/SearchByNameController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MealFridge.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +12,23 @@ namespace MealFridge.Controllers
 {
     public class SearchByNameController : Controller
     {
+        private readonly IConfiguration _configuration;
         // GET: SearchByName
+        public SearchByNameController(IConfiguration config)
+        {
+            _configuration = config;
+        }
         public ActionResult Index()
         {
+            //remove before commit 
+            //var secret = _configuration["SpoonacularPAK:PersonalAccessKey"];
+            //Debug.WriteLine("Secret: " + secret);
+            //SpoonacularAPI api = new SpoonacularAPI("https://api.spoonacular.com/recipes/complexSearch", secret);
+            //IEnumerable<Recipe> test = api.SearchAPI("rice");
+            //foreach (var i in test)
+            //{
+            //    Debug.WriteLine(i.Title);
+            //}
             return View();
         }
     }
