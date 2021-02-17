c07c7ccc79b433cef81ea00d3087cbcff936e836
Chris Edwards <56051397+chrisedwou@users.noreply.github.com>
2021-02-17 14:15:31 -0800

    Fixed account management and added API Call Model (#43)
    * Reimplemented Account Management after project remade
    
    * Fixed the account managment to what was specified on tuesday meeting. Added API model for Spoonacular API call.
    
    * Removed commented out code
    
    Co-authored-by: Joshua Utter <jutter18@mail.wou.edu>

--- MealFridge/MealFridge/Controllers/SearchByNameController.cs
+++ MealFridge/MealFridge/Controllers/SearchByNameController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MealFridge.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +12,12 @@ namespace MealFridge.Controllers
 {
     public class SearchByNameController : Controller
     {
+        private readonly IConfiguration _configuration;
         // GET: SearchByName
+        public SearchByNameController(IConfiguration config)
+        {
+            _configuration = config;
+        }
         public ActionResult Index()
         {
             return View();
