f02f752c311227c2eca8459b549a3fbd9c5d461d
cmorris19 <56059629+cmorris19@users.noreply.github.com>
2021-02-19 13:11:18 -0800

    Finished setup of Up/Down script of MealFridge as well as the scaffold

--- MealFridge/MealFridge/Startup.cs
+++ MealFridge/MealFridge/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MealFridge.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,9 +28,11 @@ namespace MealFridge
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
+            services.AddDbContext<MealFridgeDbContext>(opts =>
+            {
+                opts.UseSqlServer(Configuration["ConnectionStrings:MealFridge"]);
+            });
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
