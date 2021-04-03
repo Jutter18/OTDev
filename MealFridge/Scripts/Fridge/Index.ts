5039e1aebb4b470e030f3aa561702490b221b709
Trent Farley <trent.a.farley@gmail.com>
2021-04-03 10:14:05 -0700

    [fix] Fixed weird error where fridge/index.ts was getting the old stuff

--- MealFridge/Scripts/Fridge/Index.ts
+++ MealFridge/Scripts/Fridge/Index.ts
@@ -1,14 +1,4 @@
-﻿window.onload = () => {
-    const prevSearch = window.sessionStorage.getItem("prevSearch");
-    if (prevSearch !== null) {
-        let newSearch = <HTMLInputElement>document.getElementById("ingredSearch");
-        newSearch.value = prevSearch;
-        window.sessionStorage.clear();
-        searchByName()
-    }
-};
-
-function addIngredient(id: string, amount: string): void {
+﻿function addIngredient(id: string, amount: string): void {
     let current = parseInt($("#current-card-" + id).text(), 10);
     $("#current-card-" + id).empty()
     $("#current-card-" + id).append((current + 1).toString());//Add one for updated value
@@ -43,40 +33,6 @@ function updateInventory(id: string, amount: string): void {
         error: (err) => { console.log(err); }
     })
 }
-                        </div>
-                        <div class="card-body col-6">
-                            <h4 class="card-title">${r["name"]}</h4>
-                            <p class="card-text">Cost per Serving: ${r["cost"]}</p>
-                            <p class="card-text">Aisle: ${r["aisle"]}</p>
-                        </div>
-                        <div class="btn-group-vertical col-2" role="group">
-                            <button type="button" class="btn btn-primary addIngred" value="${r["id"]}" onclick="AddIngredient(this.value, '${r["name"]}')">Add</button>
-                        </div>
-                    </div>
-                </div>
-            `;
-        })
-    }
-}
-
-function AddIngredient(id: string, name: string): void {
-    let amount = prompt("Please enter the amount", "1");
-    if (amount != '' && !isNaN(+amount)) {
-        fetch("Fridge/AddItem?id=" + id + "&amount=" + amount, {
-            method: 'GET'
-        });
-        document.getElementById("fridge").innerHTML += `
-                        <tr id="${id}">
-                            <td class="w-75"> ${name} </td>
-                            <td class="w-25">
-                                <button type="button" class="btn btn-primary changeIngred" id="btn-${id}" value="${id}" onclick="ChangeIngredient(this.value)">
-                                    ${amount}
-                                </button>
-                            </td>                           
-                        </tr>
-        `
-    }
-}
 
 
 
@@ -106,5 +62,4 @@ inputSearchFridge.addEventListener("keydown", (e) => {
     if (e.keyCode === 13) {
         SearchByIngredientName();
     }
-
 });
